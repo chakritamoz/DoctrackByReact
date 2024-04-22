58d551110a8dca0b20680f32f26940436da26256
chakritamoz <chakrit.artamoz@gmail.com>
2024-04-22 12:57:45 +0700

    initial data privilege and role

--- server/config/initialData.js
+++ server/config/initialData.js
@@ -3,22 +3,15 @@ const Role = require('../models/role');
 
 exports.intialPrivilege = async () => {
   try {
+    // Initial privilege data
     const privileges = [
-      { name: 'createRole', description: 'Features to create new role' },
-      { name: 'editRole', description: 'Features to edit role' },
-      { name: 'deleteRole', description: 'Features to delete role' },
-      { name: 'createPrivilege', description: 'Features to create new privilege' },
-      { name: 'editPrivilege', description: 'Features to edit privilege' },
-      { name: 'deletePrivilege', description: 'Features to delete privilege' },
-      { name: 'createDocument', description: 'Features to create new documents' },
-      { name: 'editDocument', description: 'Features to edit documents' },
-      { name: 'deleteDocument', description: 'Features to delete documents' },
-      { name: 'createJob', description: 'Features to create new job' },
-      { name: 'editJob', description: 'Features to edit job' },
-      { name: 'deleteJob', description: 'Features to delete job' },
-      { name: 'createPrefix', description: 'Features to create new prefix' },
-      { name: 'editPrefix', description: 'Features to edit prefix' },
-      { name: 'deletePreifx', description: 'Features to delete prefix' },
+      { name: 'privilege-management', description: 'Features to privilege management' },
+      { name: 'role-management', description: 'Features to role management' },
+      { name: 'document-management', description: 'Features to document management' },
+      { name: 'genre-management', description: 'Features to genre management' },
+      { name: 'job-management', description: 'Features to job management' },
+      { name: 'prefix-management', description: 'Features to prefix management' },
+      { name: 'map-management', description: 'Features to map management'},
     ];
 
     await Privilege.create(privileges);
@@ -30,14 +23,47 @@ exports.intialPrivilege = async () => {
 
 // Before initial role 
 // you have to intial privilege first
-exports.initialRole = () => {
+exports.initialRole = async () => {
   try {
+    const privileges = await Privilege.find({});
+
+    // Initial role data
     const role = [
-      { name: 'owns', description: 'Full access to all features' },
-      { name: 'administrator', description: 'Access to all features except system feature'},
-      { name: 'documentKeeper', description: 'Access to only document feature' },
-      { name: 'mapKeeper', description: 'Access to only map feature'}
-    ]
+      { 
+        name: 'owns', 
+        description: 'Full access to all features',
+        privilege: privileges.map(priv => priv._id)
+      },
+      { 
+        name: 'administrator', 
+        description: 'Access to all features except system feature',
+        privilege: privileges
+          .filter(priv => 
+            priv.name !== 'role-management' || priv.name !== 'privilege-management'
+          ).map(priv => priv._id)
+      },
+      {
+        name: 'keeper',
+        description: 'Access to only document and map feature',
+        privilege: privileges
+          .filter(priv =>
+            priv.name === 'document-management' || priv.name === 'map-management'
+          ).map(priv => priv._id)
+      },
+      { 
+        name: 'documentKeeper', 
+        description: 'Access to only document feature',
+        privilege: [privileges.find(priv => priv.name === 'document-management')._id]
+      },
+      { 
+        name: 'mapKeeper',
+        description: 'Access to only map feature',
+        privilege: [privileges.find(priv => priv.name === 'map-management')._id]
+      }
+    ];
+
+    await Role.create(role);
+    console.log('Initial and saved role');
   } catch (err) {
     console.log(err);
   }
